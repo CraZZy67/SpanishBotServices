977be361a4b92b4f6e515609527e9bb85ac2c08d
CraZZy67 <sergei89066026767@gmail.com>
2025-06-19 16:15:53 +0300

    0.13.0 ADD Создал сервер для взаимодействия с интерфейсом

---
+++ agent/app/fast_server.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from agent_api import Agent
+
+
+class PromptParam(BaseModel):
+    format: str
+    level: int
+    countries: str
+    language: str
+
+app = FastAPI(debug=True)
+
+@app.post('/generate/')
+def generate_content(prompt: PromptParam):
+    return Agent.generate_post(**prompt.model_dump())
