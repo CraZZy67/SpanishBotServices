0f7a2dd6da1b1246f967880660c16003d9ee36bc
CraZZy67 <sergei89066026767@gmail.com>
2025-07-13 23:41:37 +0300

    0.18.2 REF

--- bot/app/db/queries.py
+++ bot/app/db/queries.py
@@ -74,10 +74,10 @@ def update_subscribe(payment: SuccessfulPayment, user: int):
     expiry = datetime.datetime.now() + datetime.timedelta(days=float(payment.invoice_payload))
     
     with Session(engine) as session:
-        if payment.ttotal_amount == 20000:
+        if payment.total_amount == 20000:
             session.execute(update(Payment).where(Payment.user_id == user).values(payment_id=payment.provider_payment_charge_id,
                                                                                   expiry=expiry, status='Basic'))
-        elif payment.ttotal_amount == 30000:
+        elif payment.total_amount == 30000:
             session.execute(update(Payment).where(Payment.user_id == user).values(payment_id=payment.provider_payment_charge_id,
                                                                                   expiry=expiry, status='Premium'))
         else:
